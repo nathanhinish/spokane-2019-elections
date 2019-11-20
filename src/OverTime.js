ebf1ffd0126ad5079db2fdd16d15de84d1e83c98
Nathan Hinish <nathan@triadinteractive.com>
2019-11-19 23:39:30 -0800

    First pass with simple tables.

--- src/OverTime.js
+++ src/OverTime.js
@@ -1,54 +1,100 @@
 import React from "react";
 import { sum } from "lodash";
 
-import Data from "../data/race-candidate-date-total.json";
-import Dates from "../data/dates.json";
-import Races from "../data/race-map.json";
-import Candidates from "../data/candidate-map.json";
+import Data from "./data/race-candidate-date-total.json";
+import Dates from "./data/dates.json";
+import Races from "./data/race-map.json";
+import Candidates from "./data/candidate-map.json";
 
-export default function OverTime() {
-  const races = Object.keys(Data);
-  for (const race of races) {
-    console.info(Races[race]);
-    const candidates = Object.keys(Data[race]);
-    const header = (
-      <thead>
-        <tr>
-          <td>Date</td>
-          {candidates.map((c, i) => (
-            <th key={`c${i}`}>{Candidates[c]}</th>
-          ))}
-        </tr>
-      </thead>
-    );
+function THead({ candidates }) {
+  return (
+    <thead>
+      <tr>
+        <th>Date</th>
+        {candidates.map((c, i) => (
+          <th key={`c${i}`}>{Candidates[c]}</th>
+        ))}
+      </tr>
+    </thead>
+  );
+}
 
-    const rows = [];
-    for (const date of Dates) {
-      const counts = [];
+function Table({ header, rows }) {
+  return (
+    <table>
+      {header}
+      <tbody>{rows}</tbody>
+    </table>
+  );
+}
 
-      for (const cand of candidates) {
-        const count = Data[race][cand][date];
-        counts.push(count);
-      }
-      const t = sum(counts);
+function RaceTable({ race }) {
+  const candidates = Object.keys(Data[race]);
+  const header = <THead candidates={candidates} />;
 
-      const getPct = c => Math.round((c / t) * 1000) / 10;
+  const rowData = [];
+  for (const date of Dates) {
+    const counts = [];
 
-      rows.push(
-        <tr key={`row${date}`}>
-          <td>11/{date}</td>
-          {counts.map(getPct).map((p, i) => (
-            <td key={`c${date}${i}`}>{p}</td>
-          ))}
-        </tr>
-      );
+    for (const cand of candidates) {
+      const count = Data[race][cand][date];
+      counts.push(count);
+    }
+    const t = sum(counts);
+
+    const getPct = c => Math.round((c / t) * 100000) / 1000;
+
+    rowData.push({
+      id: date,
+      date: `11/${date}`,
+      pcts: counts.map(getPct)
+    });
+  }
+
+  const rows = rowData.map(({id, date, pcts}, i) => {
+    let prevRow;
+    if (i > 0) {
+      prevRow = rowData[i - 1];
     }
 
     return (
-      <table>
-        {header}
-        <tbody>{rows}</tbody>
-      </table>
+      <tr key={`row_${id}`}>
+        <th>{date}</th>
+        {pcts.map((p, i) => {
+          let className = '';
+          if (prevRow) {
+            switch (true) {
+              case prevRow.pcts[i] > p:
+                className = 'decreased';
+                break;
+              case prevRow.pcts[i] < p:
+                className = 'increased';
+                break;
+              default: 
+              className = 'unchanged';
+            }
+          }
+          return <td key={`c${date}${i}`} className={className}>{p}</td>
+        })}
+      </tr>
     );
-  }
+  });
+
+  return (
+    <div>
+      <h3>{Races[race]}</h3>
+      <Table header={header} rows={rows} />
+    </div>
+  );
+}
+
+export default function OverTime() {
+  const races = Object.keys(Data);
+  return (
+    <>
+      {races.map((r, i) => (
+        <RaceTable race={r} key={`table${i}`} />
+      ))}
+    </>
+  );
 }
