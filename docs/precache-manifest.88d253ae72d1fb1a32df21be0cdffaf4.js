4674a6cfc7cdb5be1e6fcc19010b36bd54780d7d
Nathan Hinish <nathan@triadinteractive.com>
2019-11-19 23:50:25 -0800

    relative paths

---
+++ docs/precache-manifest.88d253ae72d1fb1a32df21be0cdffaf4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab6dac77f84befd79724ac0a2736d70b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b8f000e91ff9facc86a7",
+    "url": "./static/css/main.e399ba8f.chunk.css"
+  },
+  {
+    "revision": "a45cda1e671617683a42",
+    "url": "./static/js/2.521c62e3.chunk.js"
+  },
+  {
+    "revision": "b8f000e91ff9facc86a7",
+    "url": "./static/js/main.a9585d1d.chunk.js"
+  },
+  {
+    "revision": "922c85da6ebbfba65630",
+    "url": "./static/js/runtime-main.ab94149b.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c961947e3f45b9f2af85aa8005baa382.js"
+  "./precache-manifest.88d253ae72d1fb1a32df21be0cdffaf4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
